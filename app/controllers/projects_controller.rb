320f3bfe04979fbb345c48c6bdf7a29e43dcfc56
Luke Mayes <mayes809@gmail.com>
2022-03-29 22:46:28 +0000

    Updated tests to reach 73.08% code coverage

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,24 +1,19 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[ show edit update destroy ]
-
   # GET /projects or /projects.json
   def index
     @projects = Project.all
   end
-
   # GET /projects/1 or /projects/1.json
   def show
   end
-
   # GET /projects/new
   def new
     @project = Project.new
   end
-
   # GET /projects/1/edit
   def edit
   end
-
   # POST /projects or /projects.json
   def create
     @project = Project.new(project_params)
@@ -33,7 +28,6 @@ class ProjectsController < ApplicationController
       end
     end
   end
-
   # PATCH/PUT /projects/1 or /projects/1.json
   def update
     respond_to do |format|
@@ -46,7 +40,6 @@ class ProjectsController < ApplicationController
       end
     end
   end
-
   # DELETE /projects/1 or /projects/1.json
   def destroy
     @project.destroy
@@ -56,7 +49,6 @@ class ProjectsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
