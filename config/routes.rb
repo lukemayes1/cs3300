9e60f413cea924388c28cdbfac0d13817191b702
Luke Mayes <mayes809@gmail.com>
2022-04-26 18:10:49 +0000

    Attempted to fix logout issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "projects#index"
-  resources :projects
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+
+    root "projects#index"
+    resources :projects
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
+
+
