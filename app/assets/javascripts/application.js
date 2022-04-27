54492a62545788d3783ae141886a1b73497baa69
Luke Mayes <mayes809@gmail.com>
2022-04-27 16:41:54 +0000

    Potential port error fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,8 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
