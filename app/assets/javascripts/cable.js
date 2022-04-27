5369ee21992a5cde70f541488e455fd51cf125cb
Luke Mayes <mayes809@gmail.com>
2022-04-27 03:47:27 +0000

    Added potential fix to Procfile

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -7,13 +7,6 @@
 
 }).call(this);
 
-// app.listen(process.env.PORT || 3000, function(){
-//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-// });
-
-const PORT = proccess.env.PORT || '8080'
-
-app = express();
-
-// set the port
-app.set("port", PORT);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
