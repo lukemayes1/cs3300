ce6b801df177be1a5239dfe7a604fceba9b58693
Luke Mayes <mayes809@gmail.com>
2022-04-27 03:11:21 +0000

    Added potential fix for .js issues

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -6,3 +6,7 @@
   App.cable = ActionCable.createConsumer();
 
 }).call(this);
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
